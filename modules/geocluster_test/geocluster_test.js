bb1febe42d636870ad3891300d4161bdb66de7d3
dasjo <dasjogibtsnet@gmail.com>
2012-09-25 23:14:54 +0200

    Added javascript debugger.

---
+++ modules/geocluster_test/geocluster_test.js
@@ -0,0 +1,13 @@
+(function () {
+    var _old_initialize = L.Map.prototype.initialize;
+
+    L.Map.include({
+
+        initialize: function(/*HTMLElement or String*/ id, /*Object*/ options) {
+            // alert("loaded");
+            // this.on('load', onMapLoad, this);
+            _old_initialize.apply(this, [id, options]);
+        }
+    });
+
+}());
