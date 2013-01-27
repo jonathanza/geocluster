945a7cdcf0c4a4d267ba9ce3772cf7947afb3ab4
dasjo <dasjogibtsnet@gmail.com>
2013-01-27 18:53:55 +0100

    Enhance cluster interaction.

--- js/geocluster.leaflet.bbox.js
+++ js/geocluster.leaflet.bbox.js
@@ -3,15 +3,39 @@
   // Replace the default options for our use case.
   Drupal.leafletBBox.geoJSONOptions = {
 
-    onEachFeature: function(featureData, layer) {
-      var popupText = featureData.properties.name;
-      layer.bindPopup(popupText);
+    pointToLayer: function(featureData, latlng) {
+      if (featureData.cluster_items > 1) {
+        var icon = new L.NumberedDivIcon({number: featureData.cluster_items || 1});
+        lMarker = new L.Marker(latlng, {icon:icon});
+      }
+      else {
+        lMarker = new L.Marker(latlng);
+      }
+      return lMarker;
     },
 
-    pointToLayer: function(featureData, latlng) {
-      var icon = new L.NumberedDivIcon({number: featureData.cluster_items || 1});
-      lMarker = new L.Marker(latlng, {icon:icon});
-      return lMarker;
+    onEachFeature: function(featureData, layer) {
+      var popupText = featureData.properties.name;
+
+      if (featureData.cluster_items > 1) {
+        layer.on('click', function(e) {
+          Drupal.leafletBBox.geoJSONOptions.clickOnClustered(e, featureData, layer);
+        });
+      }
+      else {
+        layer.bindPopup(popupText);
+      }
+    },
+
+    clickOnClustered: function(e, featureData, layer) {
+        var map = layer._map;
+        // Close any other opened popup.
+        if (map._popup) {
+          map._popup._source.closePopup();
+        }
+        // Zoom and pan to clicked item.
+        map.panTo(layer.getLatLng());
+        map.zoomIn();
     }
 
   };
